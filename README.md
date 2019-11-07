18d29cdb4f27d68fd6c09d2a24384bfd30181871
acrush37 <acrush37@gmail.com>
2019-11-07 11:13:19 +0800

    LeetCode Breadth First Search

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Breadth First Search
 |993|[Cousins in Binary Tree](https://leetcode.com/problems/cousins-in-binary-tree/)|[Java](src/easy/CousinsInBinaryTree.java)|Easy|
 |107|[Binary Tree Level Order Traversal II](https://leetcode.com/problems/binary-tree-level-order-traversal-ii/)|[Java](src/easy/BinaryTreeLevelOrderTraversalII.java)|Easy|
 |1162|[As Far from Land as Possible](https://leetcode.com/problems/as-far-from-land-as-possible/)|[Java](src/medium/AsFarFromLandAsPossible.java)|Medium|
+|752|[Open the Lock](https://leetcode.com/problems/open-the-lock/)|[Java](src/medium/OpenTheLock.java)|Medium|
 |429|[N-ary Tree Level Order Traversal](https://leetcode.com/problems/n-ary-tree-level-order-traversal/)|[Java](src/medium/NryTreeLevelOrderTraversal.java)|Medium|
 |279|[Perfect Squares](https://leetcode.com/problems/perfect-squares/)|[Java](src/medium/PerfectSquares.java)|Medium|
 |103|[Binary Tree Zigzag Level Order Traversal](https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/)|[Java](src/medium/BinaryTreeZigzagLevelOrderTraversal.java)|Medium|
---
+++ src/medium/OpenTheLock.java
@@ -0,0 +1,71 @@
+package medium;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+    You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'.
+    The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+
+    The lock initially starts at '0000', a string representing the state of the 4 wheels.
+
+    You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
+
+    Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
+ */
+public class OpenTheLock {
+
+    public static void main(String... args) {
+
+        String[] deadends = {"0201", "0101", "0102", "1212", "2002"};
+        OpenTheLock openTheLock = new OpenTheLock();
+        System.out.println(openTheLock.openLock(deadends, "0202"));
+    }
+
+    private int bfs(boolean[] f, boolean[] t, int target) {
+
+        Queue<int[]> q = new LinkedList<>();
+        int[] y = new int[4];
+        int[] z = new int[4];
+        q.offer(new int[]{0, 0});
+        t[0] = true;
+
+        while (!q.isEmpty()) {
+
+            int[] x = q.poll();
+            y[0] = x[0] / 1000;
+            x[0] %= 1000;
+            y[1] = x[0] / 100;
+            x[0] %= 100;
+            y[2] = x[0] / 10;
+            y[3] = x[0] % 10;
+
+            for (int i = 0; i < 4; i++) {
+
+                for (int j = 0; j < 2; j++) {
+
+                    z = Arrays.copyOf(y, 4);
+                    z[i] = j == 0 ? (1 + z[i]) % 10 : (9 + z[i]) % 10;
+                    int u = 1000 * z[0] + 100 * z[1] + 10 * z[2] + z[3];
+                    if (u < 0 || u > 9999 || t[u] || f[u]) continue;
+                    if (u == target) return 1 + x[1];
+                    t[u] = true;
+                    q.offer(new int[]{u, 1 + x[1]});
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int openLock(String[] deadends, String target) {
+
+        boolean[] f = new boolean[10000];
+        boolean[] t = new boolean[10000];
+        for (String d : deadends) f[Integer.parseInt(d)] = true;
+        if (f[0]) return -1;
+        return bfs(f, t, Integer.parseInt(target));
+    }
+
+}
