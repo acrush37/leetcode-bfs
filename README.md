9c4229800c56d63c24c952db8814a8d17555c9da
acrush37 <acrush37@gmail.com>
2019-11-07 15:43:22 +0800

    LeetCode Breadth First Search

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Breadth First Search
 |993|[Cousins in Binary Tree](https://leetcode.com/problems/cousins-in-binary-tree/)|[Java](src/easy/CousinsInBinaryTree.java)|Easy|
 |107|[Binary Tree Level Order Traversal II](https://leetcode.com/problems/binary-tree-level-order-traversal-ii/)|[Java](src/easy/BinaryTreeLevelOrderTraversalII.java)|Easy|
 |1162|[As Far from Land as Possible](https://leetcode.com/problems/as-far-from-land-as-possible/)|[Java](src/medium/AsFarFromLandAsPossible.java)|Medium|
+|1129|[Shortest Path with Alternating Colors](https://leetcode.com/problems/shortest-path-with-alternating-colors/)|[Java](src/medium/ShortestPathWithAlternatingColors.java)|Medium|
 |1091|[Shortest Path in Binary Matrix](https://leetcode.com/problems/shortest-path-in-binary-matrix/)|[Java](src/medium/ShortestPathInBinaryMatrix.java)|Medium|
 |752|[Open the Lock](https://leetcode.com/problems/open-the-lock/)|[Java](src/medium/OpenTheLock.java)|Medium|
 |429|[N-ary Tree Level Order Traversal](https://leetcode.com/problems/n-ary-tree-level-order-traversal/)|[Java](src/medium/NryTreeLevelOrderTraversal.java)|Medium|
---
+++ src/medium/ShortestPathWithAlternatingColors.java
@@ -0,0 +1,56 @@
+package medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+    Consider a directed graph, with nodes labelled 0, 1, ..., n-1.  In this graph, each edge is either red or blue, and there could be self-edges or parallel edges.
+
+    Each [i, j] in red_edges denotes a red directed edge from node i to node j.  Similarly, each [i, j] in blue_edges denotes a blue directed edge from node i to node j.
+
+    Return an array answer of length n, where each answer[X] is the length of the shortest path from node 0 to node X such that the edge colors alternate along the path (or -1 if such a path doesn't exist).
+ */
+public class ShortestPathWithAlternatingColors {
+
+    public static void main(String... args) {
+
+        int[][] red = {{0, 1}, {1, 2}};
+        int[][] blue = {};
+        ShortestPathWithAlternatingColors shortestPathWithAlternatingColors = new ShortestPathWithAlternatingColors();
+        System.out.println(shortestPathWithAlternatingColors.shortestAlternatingPaths(3, red, blue));
+    }
+
+    private int bfs(int target, int n, boolean[][][] f) {
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][][] t = new boolean[n][n][2];
+        q.offer(new int[]{0, 0, 0});
+        q.offer(new int[]{0, 0, 1});
+
+        while (!q.isEmpty()) {
+
+            int[] x = q.poll();
+
+            for (int i = 1; i < n; i++)
+                if (!t[x[0]][i][x[2]] && f[x[0]][i][x[2]]) {
+
+                    if (i == target) return 1 + x[1];
+                    t[x[0]][i][x[2]] = true;
+                    q.offer(new int[]{i, 1 + x[1], 1 - x[2]});
+                }
+        }
+
+        return -1;
+    }
+
+    public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
+
+        int[] a = new int[n];
+        boolean[][][] f = new boolean[n][n][2];
+        for (int[] x : red_edges) f[x[0]][x[1]][0] = true;
+        for (int[] x : blue_edges) f[x[0]][x[1]][1] = true;
+        for (int i = 1; i < n; i++) a[i] = bfs(i, n, f);
+        return a;
+    }
+
+}
